395dc454d9b8d44c833503e5ed2a60aa5ddf5ae4
Bowen7 <zwxdyx@foxmail.com>
2023-05-10 22:43:26 +0800

    chore: eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["eslint:recommended", "next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
   },
