d5434d5b5045a439f581c92f819e86f11f58ef49
Bowen7 <zwxdyx@foxmail.com>
2023-05-22 22:22:07 +0800

    feat: readSequence

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,35 +1,24 @@
-import { Result, TOKEN } from "./types";
-import { ok, err, CHAR_MAP } from "./utils";
-
-const YAML_TOKEN = {
-  ...TOKEN,
-  SEQUENCE: "SEQUENCE",
-  SCALAR: "SCALAR",
-  INDENT: "INDENT",
-  DEDENT: "DEDENT",
-  LEFT_BRACKET: "LEFT_BRACKET",
-} as const;
+import { CHAR_MAP } from "./utils";
 
 type PrimitiveType = string | number | boolean | null;
 type YAMLSequence = (PrimitiveType | YAMLSequence | YAMLMap)[];
 type YAMLMap = {
   [key: string]: PrimitiveType | YAMLSequence | YAMLMap;
 };
+type MapParent = {
+  type: "map";
+  indent: number;
+  value: YAMLMap;
+};
+type SequenceParent = { type: "sequence"; indent: number; value: YAMLSequence };
+type UnknownParent = { type: "unknown"; indent: number; value: null };
 
 class Parser {
   source: string;
   size = 0;
   pos = 0;
   value: YAMLSequence | YAMLMap | null = null;
-  parents: (
-    | {
-        type: "map";
-        indent: number;
-        value: YAMLMap;
-      }
-    | { type: "sequence"; indent: number; value: YAMLSequence }
-    | { type: "unknown"; indent: number; value: null }
-  )[];
+  parents: (MapParent | SequenceParent | UnknownParent)[];
   indent = 0;
   constructor(source: string) {
     this.source = source.replace(/\r\n/g, "\n");
@@ -47,35 +36,39 @@ class Parser {
   get lastIndent() {
     return this.parents[this.parents.length - 1]?.indent || 0;
   }
+  get lastParent() {
+    return this.parents.length > 0
+      ? this.parents[this.parents.length - 1]
+      : null;
+  }
 
   parse() {
     while (!this.isEnd) {
       if (!this.readIndent()) {
         continue;
       }
-      if (this.readComment()) {
-        continue;
-      }
-      if (this.readObject()) {
-        continue;
-      }
-      const yamlKey = this.readYAMLKey();
-      if (yamlKey) {
+      if (this.peekSequence()) {
+        this.readSequence();
       }
     }
   }
 
   peekSequence() {
-    return this.char === CHAR_MAP.HYPHEN && this.nextChar === CHAR_MAP.SPACE;
-  }
-
-  peekLineBreak() {
-    return this.char === CHAR_MAP.LINE_BREAK;
+    return (
+      this.char === CHAR_MAP.HYPHEN &&
+      (this.nextChar === CHAR_MAP.SPACE ||
+        this.nextChar === CHAR_MAP.LINE_BREAK ||
+        this.nextChar === undefined)
+    );
   }
 
   readIndent() {
     const indent = this.readWhiteSpaces();
-    if (!this.peekLineBreak()) {
+    if (this.readLineBreak()) {
+      return false;
+    }
+    // just skip comment
+    if (this.readComment()) {
       return false;
     }
     const lastIndent = this.lastIndent + (this.peekSequence() ? 1 : 0);
@@ -102,6 +95,14 @@ class Parser {
       return true;
     }
     return false;
+  }
+
+  readLine() {
+    const start = this.pos;
+    while (!this.isEnd && this.char !== CHAR_MAP.LINE_BREAK) {
+      this.pos++;
+    }
+    return this.source.slice(start, this.pos).trim();
   }
 
   readComment() {
@@ -148,32 +149,60 @@ class Parser {
     if (this.source.slice(this.pos, this.pos + str.length) === str) {
       this.pos += str.length;
     } else {
-      this.error(str);
+      this.throwError(str);
     }
   }
 
-  error(message: string) {
-    throw new Error(`Expected ${message} at ${this.at()}`);
+  throwError(message: string, pos = this.pos) {
+    throw new Error(`${message} at ${this.at(pos)}`);
   }
 
-  at() {
-    const head = this.source.slice(0, this.pos + 1);
-    const tail = this.source.slice(this.pos - 1);
+  at(pos: number) {
+    const head = this.source.slice(0, pos + 1);
+    const tail = this.source.slice(pos - 1);
     const prevLinkBreak = head.lastIndexOf(CHAR_MAP.LINE_BREAK);
     const nextLinkBreak = tail.indexOf(CHAR_MAP.LINE_BREAK) || tail.length;
     return head.slice(prevLinkBreak + 1) + "^^^" + tail.slice(0, nextLinkBreak);
   }
 
-  readYAMLKey() {
+  readYAMLMapKey() {
     const scalar = this.readScalar();
     if (scalar === "") {
       return "";
     }
     this.expect(":");
     if (this.char !== CHAR_MAP.SPACE && this.char !== CHAR_MAP.LINE_BREAK) {
-      this.error("space or line break");
+      this.throwError("Expected space or line break");
     }
     return scalar;
+  }
+
+  readSequence() {
+    const start = this.pos;
+    // HYPHEN
+    this.pos++;
+    const value = this.readLine() || null;
+    if (!this.value) {
+      this.value = [value];
+      this.parents.push({
+        type: "sequence",
+        indent: this.indent,
+        value: this.value,
+      });
+    } else {
+      const lastParent = this.lastParent;
+      if (lastParent?.type === "sequence") {
+        lastParent.value.push(value);
+      } else if (lastParent?.type === "unknown") {
+        this.parents[this.parents.length - 1] = {
+          type: "sequence",
+          indent: lastParent.indent,
+          value: [value],
+        };
+      } else {
+        this.throwError("Unexpected sequence", start);
+      }
+    }
   }
 
   readObjectKey() {}
